cbb2c8b31652731c6f629382a3858c3fbe7dd628
Ayaz Mehmood <ayaz.mehmood@whetstonez.com>
2024-02-21 18:22:51 +0300

    Fixes

--- pages/bookacab/index.tsx
+++ pages/bookacab/index.tsx
@@ -61,131 +61,132 @@ const BookACab = () => {
   return (
     <>
       <NavBar />
-      <section className='my-2 flex items-center justify-center'>
+      <section className='bg-dark-navy-blue my-2 flex items-center justify-center'>
         <div className='container'>
-          <div className='flex items-center bg-[#1f32e0] rounded-3xl mx-36 justify-center overflow-y-auto overflow-x-hidden'>
-              <div className='relative max-h-full w-full max-w-md p-4'>
-                <div className='relative rounded-lg bg-[#2c40f4]'>
-                  <div className='flex items-center justify-between rounded-t p-4 md:p-5'>
-                    <h3 className='text-xl font-semibold text-secondary-foreground'>
-                      Book your cab
-                    </h3>
-                  </div>
-                  <div className='p-4 md:p-5'>
-                    <form
-                      className='space-y-4'
-                      onSubmit={(e) => {
-                        e.preventDefault();
-                        addOrEditRecord();
-                      }}
-                    >
-                      {' '}
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        Name
-                      </label>
-                      <Input
-                        type='text'
-                        name='name'
-                        className='bg-[#2f5dff] text-white placeholder:text-white focus:outline-none focus:border-none'
-                        id='name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder='name'
-                        required
-                      />
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        Email
-                      </label>
-                      <Input
-                        type='email'
-                        value={email}
-                        className='bg-[#2f5dff] text-white placeholder:text-white focus:outline-none focus:border-none'
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder='email'
-                        required
-                      />
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        WhatsApp With Coutry Code
-                      </label>
-                      <Input
-                        type='text'
-                        name='whatsapp'
-                        id='whatsapp'
-                        className='bg-[#2f5dff] text-white placeholder:text-white focus:outline-none focus:border-none'
-                        value={whatsAppWithCC}
-                        onChange={(e) => setWhatsAppWithCC(e.target.value)}
-                        placeholder='whatsapp'
-                        required
-                      />
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        Vehicle Type
-                      </label>
-                      <Select
-                        value={vehicleType}
-                        onValueChange={(newValue) => setVehicleType(newValue)}
-                      >
-                        <SelectTrigger>
-                          {
-                            vehicleTypes.find((op) => op.value === vehicleType)
-                              ?.value
-                          }
-                        </SelectTrigger>
-                        <SelectContent>
-                          {vehicleTypes.map((op) => (
-                            <SelectItem value={op.value} key={op.value}>
-                              {op.value}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        Pick Up Location
-                      </label>
-                      <Input
-                        type='text'
-                        name='name'
-                        id='name'
-                        className='bg-[#2f5dff] text-white placeholder:text-white focus:outline-none focus:border-none'
-                        value={pickUpLocation}
-                        onChange={(e) => setPickUpLocation(e.target.value)}
-                        placeholder='Pick Up Location'
-                        required
-                      />
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        Destination
-                      </label>
-                      <Input
-                        type='text'
-                        name='destination'
-                        id='destination'
-                        className='bg-[#2f5dff] text-white placeholder:text-white focus:outline-none focus:border-none'
-                        value={destination}
-                        onChange={(e) => setDestination(e.target.value)}
-                        placeholder='destination'
-                        required
-                      />
-                      <label className='mb-2 block text-sm font-medium text-secondary-foreground'>
-                        Date
-                      </label>
-                      <DatePicker date={date} setDate={setDate} />
-                      <Button
-                        type='submit'
-                        isLoading={isPending}
-                        disabled={isPending}
-                        className='w-full rounded-lg bg-[#3deac2] px-5 py-2.5 h-24 text-center text-lg font-bold text-white hover:opacity-90'
-                      >
-                        Book Now
-                      </Button>
-                    </form>
-                  </div>
+          <div className='bg-navy-blue mx-auto flex items-center justify-center overflow-y-auto overflow-x-hidden rounded-3xl'>
+            <div className='relative max-h-full w-full max-w-md p-4'>
+              <div className='bg-dark-navy-blue relative rounded-lg'>
+                <div className='flex items-center justify-between rounded-t p-4 md:p-5'>
+                  <h3 className='text-xl font-semibold text-white'>
+                    Book your cab
+                  </h3>
+                </div>
+                <div className='rounded-lg border border-gray-400 p-4 md:p-5'>
+                  <form
+                    className='space-y-4'
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      addOrEditRecord();
+                    }}
+                  >
+                    <label className='mb-2 block text-sm font-medium text-white'>
+                Name
+              </label>
+              <Input
+                type='text'
+                name='name'
+                className='bg-navy-blue text-black placeholder-gray-400 focus:outline-none focus:border-none'
+                id='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='Name'
+                required
+              />
+              <label className='mb-2 block text-sm font-medium text-white'>
+                Email
+              </label>
+              <Input
+                type='email'
+                value={email}
+                className='bg-navy-blue text-black placeholder-gray-400 focus:border-none focus:outline-none'
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+                required
+              />
+              <label className='mb-2 block text-sm font-medium text-white'>
+                WhatsApp With Country Code
+              </label>
+              <Input
+                type='text'
+                name='whatsapp'
+                id='whatsapp'
+                className='bg-navy-blue text-black placeholder-gray-400 focus:border-none focus:outline-none'
+                value={whatsAppWithCC}
+                onChange={(e) => setWhatsAppWithCC(e.target.value)}
+                placeholder='WhatsApp With Country Code'
+                required
+              />
+              <label className='mb-2 block text-sm font-medium text-white'>
+                Vehicle Type
+              </label>
+              <Select
+                value={vehicleType}
+                onValueChange={(newValue) => setVehicleType(newValue)}
+              >
+                <SelectTrigger>
+                  {
+                    vehicleTypes.find((op) => op.value === vehicleType)
+                      ?.value
+                  }
+                </SelectTrigger>
+                <SelectContent>
+                  {vehicleTypes.map((op) => (
+                    <SelectItem value={op.value} key={op.value}>
+                      {op.value}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <label className='mb-2 block text-sm font-medium text-white'>
+                Pick Up Location
+              </label>
+              <Input
+                type='text'
+                name='pickUpLocation'
+                id='pickUpLocation'
+                className='bg-navy-blue text-black placeholder-gray-400 focus:border-none focus:outline-none'
+                value={pickUpLocation}
+                onChange={(e) => setPickUpLocation(e.target.value)}
+                placeholder='Pick Up Location'
+                required
+              />
+              <label className='mb-2 block text-sm font-medium text-white'>
+                Destination
+              </label>
+              <Input
+                type='text'
+                name='destination'
+                id='destination'
+                className='bg-navy-blue text-black placeholder-gray-400 focus:border-none focus:outline-none'
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+                placeholder='Destination'
+                required
+              />
+              <label className='mb-2 block text-sm font-medium text-white'>
+                Date
+              </label>
+              <DatePicker
+                date={date}
+                setDate={setDate}
+              />
+              <Button
+                type='submit'
+                isLoading={isPending}
+                disabled={isPending}
+                className='w-full rounded-lg bg-teal-500 px-5 py-2.5 text-lg font-bold text-white hover:bg-teal-600'
+              >
+                Book Now
+              </Button>
+                  </form>
                 </div>
               </div>
             </div>
-          
+          </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default BookACab
